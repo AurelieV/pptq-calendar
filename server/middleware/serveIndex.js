79a97601e89553757673e40f6e7f1c2aeb482210
Aurélie Violette <aurelie.violette@purple-fox.fr>
2016-08-07 14:22:23 +0200

    Use webpack config for client src. Todo: make it ok for production mode

--- server/middleware/serveIndex.js
+++ server/middleware/serveIndex.js
@@ -3,6 +3,6 @@ var path = require('path');
 module.exports = function () {
   "use strict";
   return function (req, res) {
-    res.sendFile(path.resolve('www', 'index.html'));
+    res.sendFile(path.resolve('dist', 'index.html'));
   }
 };
