1b10905abedd6a35a9f6a9807416cb9fa8ebe6cf
Aurélie Violette <aurelie.violette@purple-fox.fr>
2016-08-09 16:19:10 +0200

    Enable cookie authentication

--- server/pptq-calendar.js
+++ server/pptq-calendar.js
@@ -20,6 +20,9 @@ app.start = function() {
   });
 };
 
+// Activate cookie authentication
+app.use(loopback.token({ model: app.models.accessToken }));
+
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
