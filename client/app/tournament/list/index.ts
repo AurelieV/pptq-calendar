3673f844625593fcc8627b1facf1f3024527cbbd
Aurélie VIOLETTE <orwel.violette@gmail.com>
2016-12-10 00:27:25 +0100

    Improve tournament list design

--- client/app/tournament/list/index.ts
+++ client/app/tournament/list/index.ts
@@ -34,26 +34,32 @@ export class TournamentListComponent implements OnInit, OnDestroy {
       const tmp = _.groupBy(tournaments, (t) => moment.utc(t.date).format('YYYY-W'));
       let byWeek = [];
       Object.keys(tmp).forEach((w) => {
-        let tmp2 = _.groupBy(tmp[w], (t) => moment.utc(t.date).format('dddd'));
-        let byDay = [];
-        Object.keys(tmp2).forEach((d) => {
-          byDay.push({
-            day: d,
-            tournaments: tmp2[d]
-          });
-        });
         const m = moment(w, 'YYYY-W');
         byWeek.push({
           week: {
             start: m.startOf('week').format('DD MMM'),
             end: m.endOf('week').format('DD MMM')
           },
-          days: byDay
+          days: this.groupByDay(tmp[w], w)
         });
       });
       this.weeks = byWeek;
     }));
     this.tournamentsActions.fetchTournaments();
+  }
+
+  groupByDay(tab: any[], week: string) {
+    let tmp = _.groupBy(tab, (t) => moment.utc(t.date).format('E'));
+    tmp = Object.assign({'6': [], '7': []}, tmp);
+    let result = [];
+    Object.keys(tmp).forEach((d) => {
+      result.push({
+        day: moment.utc(`${week}-${d}`, 'YYYY-W-E').format('dddd DD MMM'),
+        tournaments: tmp[d]
+      });
+    });
+
+    return result;
   }
 
   ngOnDestroy() {
