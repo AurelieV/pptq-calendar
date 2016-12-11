f825d99f2ae9d712e7843e67a16be154b23c13cf
Aurélie VIOLETTE <orwel.violette@gmail.com>
2016-12-11 14:12:21 +0100

    Add register form design

--- client/app/register/index.ts
+++ client/app/register/index.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  template : require('./register.html')
+  template : require('./register.html'),
+  styles: [ require('./register.scss') ]
 })
 export class RegisterComponent {
 
